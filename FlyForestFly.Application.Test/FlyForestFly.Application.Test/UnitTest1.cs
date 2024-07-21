6b45b75ff8fa03e804e44cffc019f2c9350cfbe7
=Antony <guilhermecosta994@gmail.com>
2024-07-21 15:22:29 -0300

    Create the Application Section

---
+++ FlyForestFly.Application.Test/FlyForestFly.Application.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace FlyForestFly.Application.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
